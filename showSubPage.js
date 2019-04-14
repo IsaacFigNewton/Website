3a7afdda6b74c7f8e26ff820354370d849ffd6f5
IsaacFigNewton <48895941+IsaacFigNewton@users.noreply.github.com>
2019-04-13 19:25:33 -0700

    tried again

--- showSubPage.js
+++ showSubPage.js
@@ -1,5 +1,2 @@
 //I'm using it to refactor the header and footer code across my webpages
 //Basically, what I want to do is hide all other "sub-pages" on a site when the viewer clicks on a link in the header.
-function showOnly() {
-	
-}
